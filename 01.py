2bbbc7d45ef41e4bec7cdbf3fd5f65bfc3295ad6
zhangshengshan <zhangshengshan@youzan.com>
2017-04-18 13:52:51 +0800

    tmp ci

---
+++ 01.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("test/", one_hot=True)
+print(mnist.train.images.shape, mnist.train.labels.shape)
+print(mnist.test.images.shape, mnist.test.labels.shape)
+print(mnist.validation.images.shape, mnist.validation.labels.shape)
+
+sess = tf.InteractiveSession()
+
+x = tf.placeholder(tf.float32, [None, 784])
+
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+cross_entropy = tf.reduce_mean( -tf.reduce_sum( y_ * tf.log(y), reduction_indices=[1]))
+
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+sess = tf.InteractiveSession()
+
+
+tf.global_variables_initializer().run()
+
+for _ in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
