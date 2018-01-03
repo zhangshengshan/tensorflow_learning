2403b0e9c10f26cc4ea0a9486dca999a94231f63
zhangshengshan <zhangshengshan@youzan.com>
2018-01-03 13:47:13 +0800

    tmp ci

--- 01.py
+++ 01.py
@@ -1,10 +1,22 @@
+import argparse
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
+parser = argparse.ArgumentParser()
+
+#配置训练数据的地址
+parser.add_argument('--data_dir', type=str, default='', help='input data path')
+#配置模型保存地址
+parser.add_argument('--model_dir', type=str, default='', help='output model path')
+
+FLAGS, _ = parser.parse_known_args()
+print(FLAGS)
 
 mnist = input_data.read_data_sets("test/", one_hot=True)
 print(mnist.train.images.shape, mnist.train.labels.shape)
 print(mnist.test.images.shape, mnist.test.labels.shape)
 print(mnist.validation.images.shape, mnist.validation.labels.shape)
+
 
 sess = tf.InteractiveSession()
 
@@ -18,10 +30,8 @@ y_ = tf.placeholder(tf.float32, [None, 10])
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 cross_entropy = tf.reduce_mean( -tf.reduce_sum( y_ * tf.log(y), reduction_indices=[1]))
 
-
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 sess = tf.InteractiveSession()
-
 
 tf.global_variables_initializer().run()
 
---
+++ 02.py
@@ -0,0 +1,33 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import  tensorflow as tf
+mnist = input_data.read_data_sets("./", one_hot=True )
+sess = tf.InteractiveSession()
+
+
+in_units = 784 
+h1_units = 300
+
+W1 = tf.Variable(tf.truncated_normal([in_units, h1_units], stddev = 0.1))
+b1 = tf.Variable(tf.zeros([h1_units]))
+
+W2 = tf.Variable(tf.zeros([h1_units,10])) 
+b2 = tf.Variable(tf.zeros(10))
+
+x = tf.placeholder(tf.float32, [None, in_units])
+keep_prob = tf.placeholder(tf.float32)
+
+hidden1 = tf.nn.relu(tf.matmul(x, W1)+b1)
+hidden1_drop = tf.nn.dropout(hidden1, keep_prob)
+
+y = tf.nn.softmax(tf.matmul( hidden1_drop, W2 ) +b2)
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean( -tf.reduce_sum( y_ * tf.log(y), reduction_indices=[1] ) )
+
+train_step = tf.train.AdagradOptimizer(0.3).minimize(cross_entropy)
+
+sess.run(tf.global_variables_initializer())
+
+for i in range(3000):
+    batch_xs, batch_ys =  mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x :batch_xs, y: batch_ys, keep_prob: 0.75})
